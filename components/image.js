eb8b3c89418ad6cfeb00f5164005f4539b9abd4e
wutchara <SSTMUHAM@gmail.com>
2021-04-06 22:19:58 +0700

    Add new components

---
+++ components/image.js
@@ -0,0 +1,3 @@
+const Image = () => (<img src="http://www.metalbridges.com/wp-content/uploads/2014/07/Sword-Art-Online-511.jpg" />);
+
+export default Image;
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import Image from '../components/image';
 
 const AboutPage = () => (<div style={{fontSize: '20px', color: 'blue'}}>
     <h1>About Page</h1>
     <Link href='/'>
         <button>Back</button>
     </Link>
+    <Image />
     <p>I was a magician once</p>
 </div>);
 
